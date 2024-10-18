f12cca3e75e49c4df7e50531a6308edcd12b3256
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-18 22:44:15 +0100

    chore(setup): basic setup

--- bin/dart_fsm.dart
+++ bin/dart_fsm.dart
@@ -1,5 +1,5 @@
 import 'package:dart_fsm/dart_fsm.dart' as dart_fsm;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_fsm.calculate()}!');
+  print("hello finite state machine");
 }
--- test/dart_fsm_test.dart
+++ test/dart_fsm_test.dart
@@ -1,8 +1,2 @@
 import 'package:dart_fsm/dart_fsm.dart';
 import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
