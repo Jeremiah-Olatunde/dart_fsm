105f7d846e313cc6321a779e01bf103cf630ffe2
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-19 00:16:06 +0100

    feat(turnstile): move to const constructors

--- lib/turnstile.dart
+++ lib/turnstile.dart
@@ -1,11 +1,40 @@
 sealed class State {}
 
-class Locked implements State {}
+class Locked implements State {
+  const Locked();
+}
 
-class Unlocked implements State {}
+class Unlocked implements State {
+  const Unlocked();
+}
 
 sealed class Event {}
 
-class Push implements Event {}
+class Push implements Event {
+  const Push();
+}
 
-class Coin implements Event {}
+class Coin implements Event {
+  const Coin();
+}
+
+State nextState(State state, Event event) {
+  return switch (state) {
+    Locked() => isLocked(event),
+    Unlocked() => isUnlocked(event),
+  };
+}
+
+State isLocked(Event event) {
+  return switch (event) {
+    Push() => const Locked(),
+    Coin() => const Unlocked(),
+  };
+}
+
+State isUnlocked(Event event) {
+  return switch (event) {
+    Push() => const Locked(),
+    Coin() => const Unlocked(),
+  };
+}
