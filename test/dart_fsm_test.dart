fbb403abacaf274525b23ebe890de5e0bf1fc41b
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-19 00:23:10 +0100

    test(turnstile): add state-transition table test

--- test/dart_fsm_test.dart
+++ test/dart_fsm_test.dart
@@ -1,2 +1,26 @@
-import 'package:dart_fsm/dart_fsm.dart';
+import 'package:dart_fsm/turnstile.dart';
 import 'package:test/test.dart';
+
+void main() {
+  group("finite state machine", () {
+    group("state(locked)", () {
+      test("input(coin)", () {
+        expect(nextState(const Locked(), const Coin()), const Unlocked());
+      });
+
+      test("input(push)", () {
+        expect(nextState(const Locked(), const Push()), const Locked());
+      });
+    });
+
+    group("state(unlocked)", () {
+      test("input(coin)", () {
+        expect(nextState(const Unlocked(), const Coin()), const Unlocked());
+      });
+
+      test("input(push)", () {
+        expect(nextState(const Unlocked(), const Push()), const Locked());
+      });
+    });
+  });
+}
