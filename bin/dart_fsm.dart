32c72e1e34d449f22acb665229338d89dee4708a
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-18 22:32:52 +0100

    chore(setup): initial commit

---
+++ bin/dart_fsm.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fsm/dart_fsm.dart' as dart_fsm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fsm.calculate()}!');
+}
---
+++ test/dart_fsm_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fsm/dart_fsm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
