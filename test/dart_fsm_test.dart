1c6275eff140ab277810a860d5c66f84e0895732
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-23 23:02:54 +0100

    test(binary_parser): add tests for all state and transition combinations

--- test/dart_fsm_test.dart
+++ test/dart_fsm_test.dart
@@ -1,4 +1,5 @@
 import 'package:dart_fsm/turnstile.dart' as ts;
+import 'package:dart_fsm/binary_parser.dart' as bp;
 import 'package:test/test.dart';
 
 void main() {
@@ -22,5 +23,31 @@ void main() {
         expect(ts.nextState(const ts.Unlocked(), const ts.Push()), const ts.Locked());
       });
     });
+  });
+
+  group('binary parser', () {
+    test("state(1) | event(0)", (){
+      expect(bp.nextState(const bp.One(), 0), const bp.Zero());
+    });
+
+    test("state(0) | event(1)", (){
+      expect(bp.nextState(const bp.Zero(), 1), const bp.One());
+    });
+
+    test("state(1) | event(1)", (){
+      expect(bp.nextState(const bp.One(), 1), const bp.Err());
+    });
+
+    test("state(0) | event(0)", (){
+      expect(bp.nextState(const bp.Zero(), 0), const bp.Err());
+    });    
+
+    test("state(error) | event(1)", (){
+      expect(bp.nextState(const bp.Err(), 1), const bp.Err());
+    });
+
+    test("state(error) | event(0)", (){
+      expect(bp.nextState(const bp.Err(), 0), const bp.Err());
+    });    
   });
 }
