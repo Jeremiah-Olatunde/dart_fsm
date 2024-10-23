5958f6e87d6e34a53369d2b745a59c851e858e37
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-23 22:50:39 +0100

    feat(binary_parser): add state transition function

--- lib/binary_parser.dart
+++ lib/binary_parser.dart
@@ -12,3 +12,27 @@ class Zero implements State {
 class Err implements State {
   const Err();
 }
+
+State eventOne(State state){
+  return switch(state) {
+    Err() => const Err(),
+    One() => const Err(),
+    Zero() => const One(),
+  };
+}
+
+State eventZero(State state){
+  return switch(state) {
+    Err() => const Err(),
+    One() => const Zero(),
+    Zero() => const Err(),
+  };
+}
+
+State bitToEvent(State state, int n){
+  return switch(n){
+    0 => eventZero(state),
+    1 => eventOne(state),
+    _ => throw "non-binary digit"
+  };
+}
